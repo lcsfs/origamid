0f98cedd8659d831313b46d8e19e6797a6b55f5d
Lucas Ferreira <lucas_ferreir.a@outlook.com>
2021-07-01 17:45:59 -0300

    add folder exercism

---
+++ exercicios/01/script.js
@@ -0,0 +1,15 @@
+// Declarar uma variável com o seu nome
+var myName = "Lucas";
+
+// Declarar uma variável com a sua idade
+var age = 23;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var favFood;
+
+// Atribuir valor a sua comida favorita
+favFood = "Pizza";
+
+// Declarar 5 variáveis diferentes sem valores
+var one, two, three, four, five;
