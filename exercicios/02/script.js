a3aa2ca5504bcf7e7341e88c141bd0d3f67ee94f
Lucas Ferreira <lucas_ferreir.a@outlook.com>
2021-07-01 19:49:40 -0300

    ex-02

---
+++ exercicios/02/script.js
@@ -0,0 +1,19 @@
+// Declare uma variável contendo uma string
+var myName = "Lucas"
+
+// Declare uma variável contendo um número dentro de uma string
+var num = "23"
+
+// Declare uma variável com a sua idade
+var age = 23
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var midName = 'Ferreira';
+console.log(`${myName} ${midName}`);
+
+// Coloque a seguinte frase em uma variável: It's time
+var word = `It's time`
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof myName)
