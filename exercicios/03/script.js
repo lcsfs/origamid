b3331e84ee896050f0925b39ca86802525f8081c
Lucas Ferreira <lucas_ferreir.a@outlook.com>
2021-07-01 20:48:05 -0300

    ex-03

---
+++ exercicios/03/script.js
@@ -0,0 +1,21 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total); // = 35
+
+// Crie duas expressões que retornem NaN
+var exp1 = 'test' - 2; // NaN
+var exp2 = 'test' / 3; // NaN
+
+// Somar a string '200' com o número 50 e retornar 250
+var sum = +'200' + 50;
+console.log(sum); // 250
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+console.log(++x);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+console.log(peso);
