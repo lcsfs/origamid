b8320c238726999000a1d6dd8e9a132a90e3ca02
Lucas Ferreira <lucas_ferreir.a@outlook.com>
2021-07-12 17:47:06 -0300

    ex-10

---
+++ exercicios/10/script.js
@@ -0,0 +1,12 @@
+// Retorne o url da página atual utilizando o objeto window
+const hrefPage = location.href;
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const activeElement = document.querySelector(".ativo");
+
+// Retorne a linguagem do navegador
+const languagePage = navigator.language;
+
+// Retorne a largura da janela
+const widthPage = innerWidth;
