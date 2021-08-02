4f6b74e35bfd13eb245031a2f8938ced43a9ca75
Lucas Ferreira <lucas_ferreir.a@outlook.com>
2021-08-02 20:26:59 -0300

    ex-12

---
+++ exercicios/12/script.js
@@ -0,0 +1,22 @@
+// Mostre no console cada parágrado do site
+const paragraph = document.querySelectorAll('p');
+console.log(paragraph);
+// Mostre o texto dos parágrafos no console
+paragraph.forEach((item) => {
+  console.log(item.textContent);
+})
+
+// Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
+imgs.forEach(() => i++);
+
