98caf81439b0aa2b5d9add465c104520424f4eef
Lucas Ferreira <lucas_ferreir.a@outlook.com>
2021-08-30 17:04:46 -0300

    ex-14

---
+++ exercicios/14/script.js
@@ -0,0 +1,47 @@
+// Quando o usuário clicar nos links internos do site,
+// adicione a classe ativo ao item clicado e remova dos
+// demais itens caso eles possuam a mesma. Previna
+// o comportamento padrão desses links
+const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+function handleLink(event) {
+  event.preventDefault();
+  linksInternos.forEach((link) => {
+    link.classList.remove('ativo')
+  })
+  event.currentTarget.classList.add('ativo');
+};
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', handleLink)
+})
+
+// Selecione todos os elementos do site começando a partir do body,
+// ao clique mostre exatamente quais elementos estão sendo clicados
+const allElements = document.querySelectorAll('body *');
+
+function handleElement(event) {
+  console.log(event.currentTarget)
+}
+
+allElements.forEach((show) => {
+  show.addEventListener('click', handleElement);
+})
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+
+/* function handleElement(event) {
+  event.currentTarget.remove()
+} */
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
+
+function handleClickT(event) {
+  console.log(event.key);
+  if (event.key === 't') {
+    document.documentElement.classList.toggle('textMaior');
+  }
+};
+
+window.addEventListener('keydown', handleClickT);
